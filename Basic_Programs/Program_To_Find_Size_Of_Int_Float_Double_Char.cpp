1509065eeb189be354b4d9806e041509be33c535
Shrutipatil23 <patilshrutis7597@gmail.com>
2023-02-23 11:29:58 +0530

    Basic Program

---
+++ Basic_Programs/Program_To_Find_Size_Of_Int_Float_Double_Char.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"SIZE OF INT DATATYPE = "<<(sizeof(int))<<endl;
+    cout<<"SIZE OF FLOAT DATATYPE = "<<(sizeof(float))<<endl;
+    cout<<"SIZE OF DOUBLE DATATYPE = "<<(sizeof(double))<<endl;
+    cout<<"SIZE OF CHAR DATATYPE = "<<(sizeof(char))<<endl;
+    return 0;
+}
