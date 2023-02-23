20b80642fc2e769fe5009d44c7acba5398dd1314
Shrutipatil23 <patilshrutis7597@gmail.com>
2023-02-23 11:47:07 +0530

    Basic Program

---
+++ Basic_Programs/Program_To_Find_ASCII_Value_Of_Character.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+
+    cout<<"ENTER ANY CHARACTER : ";
+    cin>>ch;
+
+    cout<<"THE ASCII VALUE FOR GIVEN CHARACTER "<<ch<<" = "<<int(ch);
+    
+    return 0;
+}
