2ffa8922add0b90226a1c32c6996261d201ea042
Shrutipatil23 <patilshrutis7597@gmail.com>
2023-02-23 11:36:54 +0530

    Basic Program

---
+++ Basic_Programs/Program_To_Multiply_Two_Numbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1=0,num2=0,product=0;
+
+    cout<<"ENTER FIRST NUMBER = ";
+    cin>>num1;
+    cout<<"ENTER SECOND NUMBER = "<<endl;
+    cin>>num2;
+
+    product = num1*num2;
+
+    cout<<"MULTIPLICATION OF TWO NUMBERS = "<<product;
+    return 0;
+}
